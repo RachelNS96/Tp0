dde5d477011cfc58f5d2ee908eb852f5eaf0399a
Carlos Contreras <86571277+charlymil979@users.noreply.github.com>
2023-04-18 19:42:28 -0300

    Add files via upload
    actualizacion de JS

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,51 +1,45 @@
-const d= document;
+const d = document;
 
-function darkButton() {
-  $body = document.querySelector("body")
-  $dark = d.createElement("button")
-  $dark.classList.add("dark-mode")
-  $dark.textContent="🌙";
-  $body.insertAdjacentElement("afterbegin", $dark)
+export function darkButton() {
+  const $body = document.querySelector("body"),
+    $dark = d.createElement("button");
+    
+  $dark.classList.add("dark-mode");
+  $dark.textContent = "🌙";
+  $body.insertAdjacentElement("afterbegin", $dark);
 }
 
+export function darkMode() {
+  const $selectors = d.querySelectorAll(".light"),
+    $button = d.querySelector(".dark-mode"),
+    isactive = localStorage.getItem("darkmode") || 0,
+    moon = "🌙",
+    sun = "☀️";
 
-function darkMode(){
-   const $selectors = d.querySelectorAll(".light"),
-     $button = d.querySelector(".dark-mode"),
-     isactive = localStorage.getItem("darkmode") || 0
-     moon = "🌙",
-     sun = "☀️";
-
-   const darkactive=function(){
+  const darkactive = function () {
       $selectors.forEach((el) => {
         el.classList.add("dark");
       });
       $button.textContent = sun;
       localStorage.setItem("darkmode", 1);
-   },
-   light=function(){
+    },
+    light = function () {
       $selectors.forEach((el) => {
         el.classList.remove("dark");
       });
       $button.textContent = moon;
       localStorage.setItem("darkmode", 0);
-   }
-   //este if es para comprobar si quedo activado en memoria
-if(isactive==='1') darkactive();
+    };
+  //este if es para comprobar si quedo activado en memoria
+  if (isactive === "1") darkactive();
 
-d.addEventListener(('click'),(e)=>{
-   //(`${button} *`)implica todos los elementos dentro de button
-   if(e.target.matches(".dark-mode")||(e.target.matches(`${button} *`))){
-// d.querySelector('body').classList.toggle(darkMode),esto es muy estatico, con data-attributes es mas fluido, solo hay que agregar data-dark a cada elemento a aplicar el darkMode
-   if($button.textContent===moon){
-      darkactive();
-         }
-         else{
-            light()}
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(".dark-mode") || e.target.matches(`.dark-mode *`)) {
+      if ($button.textContent === moon) {
+        darkactive();
+      } else {
+        light();
+      }
+    }
+  });
 }
-})
-}
-d.addEventListener("DOMContentLoaded", e =>{
-  darkButton();
-  darkMode()
-})
