b0b242b47ab43eaffc4b2e2434643f3735b6acae
Aaron <49597832+D41mr14n@users.noreply.github.com>
2023-04-24 20:45:49 -0300

    Add files via upload

---
+++ js/dark_mode.js
@@ -0,0 +1,45 @@
+const d = document;
+
+export function darkButton() {
+  const $body = document.querySelector("body"),
+    $dark = d.createElement("button");
+    
+  $dark.classList.add("dark-mode");
+  $dark.textContent = "🌙";
+  $body.insertAdjacentElement("afterbegin", $dark);
+}
+
+export function darkMode() {
+  const $selectors = d.querySelectorAll(".light"),
+    $button = d.querySelector(".dark-mode"),
+    isactive = localStorage.getItem("darkmode") || 0,
+    moon = "🌙",
+    sun = "☀️";
+
+  const darkactive = function () {
+      $selectors.forEach((el) => {
+        el.classList.add("dark");
+      });
+      $button.textContent = sun;
+      localStorage.setItem("darkmode", 1);
+    },
+    light = function () {
+      $selectors.forEach((el) => {
+        el.classList.remove("dark");
+      });
+      $button.textContent = moon;
+      localStorage.setItem("darkmode", 0);
+    };
+  //este if es para comprobar si quedo activado en memoria
+  if (isactive === "1") darkactive();
+
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(".dark-mode") || e.target.matches(`.dark-mode *`)) {
+      if ($button.textContent === moon) {
+        darkactive();
+      } else {
+        light();
+      }
+    }
+  });
+}
